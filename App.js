4a79620cff91a63b668cd8e0ce92c26dd77ea9e2
sevvalseray <sevvalseray@hotmail.com>
2021-01-31 19:54:53 +0300

    tab navigator's added

--- App.js
+++ App.js
@@ -2,14 +2,22 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Login from './src/auth/Login'
 import { useState } from 'react';
 import { useEffect } from 'react';
 import Register from './src/auth/Register';
 import Home from './src/dashboard/Home'
+import Search from './src/dashboard/Search'
+import NewReview from './src/dashboard/NewReview'
+import ReviewList from './src/dashboard/ReviewList'
+import Profile from './src/dashboard/Profile'
+import { Ionicons } from '@expo/vector-icons'
 
+import i18n from './src/config/i18n';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
 
@@ -21,12 +29,58 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName={initialRoute}>
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Register" component={Register} />
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name={i18n.t('dashboard.homepage')} component={MainTabNavigator} />
+        <Stack.Screen name={i18n.t('login')} component={Login} />
+        <Stack.Screen name={i18n.t('register')} component={Register} />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
+}
+
+function MainTabNavigator() {
+  return (
+    <Tab.Navigator
+      tabBarOptions={{
+        activeTintColor: '#384F7D',
+        inactiveTintColor: '#A5B0C4', 
+        style: {
+          backgroundColor: '#ffffff'
+        }
+      }}
+    >
+      <Tab.Screen name={i18n.t('dashboard.homepage')} component={Home}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name={'ios-home'} color={color} size={size} />
+          ),
+        }} />
+      <Tab.Screen name={i18n.t('dashboard.search')} component={Search}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name={'search-outline'} color={color} size={size} />
+          ),
+        }} />
+      <Tab.Screen name={i18n.t('dashboard.new')} component={NewReview}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name={'add-outline'} color={color} size={size} />
+          ),
+        }} />
+        <Tab.Screen name={i18n.t('dashboard.reviews')} component={ReviewList}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name={'bulb-outline'} color={color} size={size} />
+          ),
+        }} />
+        <Tab.Screen name={i18n.t('dashboard.profile')} component={Profile}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name={'person-outline'} color={color} size={size} />
+          ),
+        }} />
+    </Tab.Navigator>
+  )
 }
 
 const styles = StyleSheet.create({
---
+++ src/dashboard/NewReview.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View,Text } from 'react-native';
+import Layout from '../../components/Layout/Layout';
+
+const NewReview = () => {
+    return (
+        <Layout title={"Süleyman"} back>
+            <Text>Yeni İnceleme Ekleme Sayfası</Text>
+        </Layout>
+    )
+}
+
+export default NewReview;
