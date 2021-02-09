2d4e99720d4b977917bb680b5f6e9fea621c0dc4
suleyman <suleyman.sevimli1@gmail.com>
2021-02-09 15:49:42 +0300

    removed unused imported elements

--- src/dashboard/Home.js
+++ src/dashboard/Home.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { View,Text } from 'react-native';
+import { Text } from 'react-native';
 import Layout from '../../components/Layout/Layout';
 
 const Home = () => {
     return (
         <Layout back>
-            <Text>Ana Sayfası</Text>
+            <Text>Ana Sayfa</Text>
         </Layout>
     )
 }
