6c8757cb09dcb6f56eb50ce52113aa560bdcdee2
suleyman <suleyman.sevimli1@gmail.com>
2021-02-15 09:19:42 +0300

    add review redux

--- src/dashboard/SubScreens/AddReview.js
+++ src/dashboard/SubScreens/AddReview.js
@@ -1,16 +1,25 @@
 import React, { useEffect } from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 import Layout from '../../../components/Layout/Layout';
+import { useDispatch, useSelector } from 'react-redux';
+import { selfLink } from '../../store/reducers/bookReducer';
 
 const AddReview = ({ route, navigation }) => {
 
+  const state = useSelector(state => state.book);
+  const dispatch = useDispatch()
+
   useEffect(() => {
 
-  },[]);
+    dispatch(selfLink(route.params?.self))
+
+  }, []);
 
   return (
-    <Layout navigation={navigation} back title={"Add Book Review"} >
+    <Layout navigation={navigation} back title={'Add Book Review'}>
+      <View>
 
+      </View>
     </Layout>
   );
 };
